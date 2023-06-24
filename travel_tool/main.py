3e04ff99700abda872f96f3f82133e89c503f8b7
Evan Lin <evan.if.lin@linecorp.com>
2023-06-24 19:22:35 +0800

    feat: add DuckDuckGo search tool to main.py tools list
    - Add a new tool for DuckDuckGo search to the `tools` list in `main.py` file.
    
    Signed-off-by: Evan Lin <evan.if.lin@linecorp.com>

--- travel_tool/main.py
+++ travel_tool/main.py
@@ -7,6 +7,8 @@ from langchain.chat_models import ChatOpenAI
 from langchain.agents import AgentType
 from langchain.agents import initialize_agent, Tool
 from langchain.schema import HumanMessage
+from langchain.tools import DuckDuckGoSearchRun
+
 from poi import TravelPOITool
 from ticket import TravelTicketTool
 from exp import TravelExpTool
@@ -16,7 +18,7 @@ from product import ProductTool
 model = ChatOpenAI(model="gpt-3.5-turbo-0613")
 
 tools = [TravelPOITool(), TravelTicketTool(),
-         TravelExpTool(), WeatherDataTool(), ProductTool()]
+         TravelExpTool(), WeatherDataTool(), ProductTool(), DuckDuckGoSearchRun()]
 
 open_ai_agent = initialize_agent(tools,
                                  model,
